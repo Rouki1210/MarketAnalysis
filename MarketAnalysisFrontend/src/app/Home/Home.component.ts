a156eae1bba0a1376551eb965a4953be86d65042
Vinh Nguyen <vinh12102004@gmail.com>
2025-09-11 08:51:09 +0700

    test endpoint

---
+++ MarketAnalysisFrontend/src/app/Home/Home.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Home',
+  templateUrl: './Home.component.html',
+  imports: [CommonModule],
+  styleUrls: ['./Home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.loadData();
+  }
+  data: any = {};
+
+    loadData() {
+    this.http.get('https://localhost:7175/api/Asset').subscribe((a: any) => {
+      this.data = a;
+      console.log(a);
+    });
+  }
+}
