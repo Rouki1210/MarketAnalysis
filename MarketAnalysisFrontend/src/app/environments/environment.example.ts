0b3e14ff6508a03b1cf4ee8be5ee2809fe6c4aa0
25thnovvv <25th.novvv@gmail.com>
2025-10-20 12:09:11 +0700

    Update

--- MarketAnalysisFrontend/src/app/environments/environment.example.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'https://localhost:7175',
-  googleRedirectUri: 'http://localhost:4200',
-  googleClientId: 'YourGoogleClientIdHere'
-};
